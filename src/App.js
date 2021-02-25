a1fff07c6d11ac3c6401aeb7c8a6562417f8fa77
A Prashanth Reddy <63457391+PrashanthreddyAinala@users.noreply.github.com>
2021-02-25 08:30:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Header from "./Header";
+import MemeGenerator from "./MemeGenerator";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <MemeGenerator />
+    </div>
+  );
+}
